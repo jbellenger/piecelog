2d43d7060a1d2d29cb39e9e881e49275b5d2e5d7
jbellenger <jbellenger@twitter.com>
2016-03-14 09:24:51 -0700

    load alasql from cloudflare

--- src/client/modules/query/index.js
+++ src/client/modules/query/index.js
@@ -1,6 +1,5 @@
 // Alasql doesn't work well with webpack since alasql uses "exports" to
 // detected whether or not it's in node.js or the browser
-import alasql from 'script!alasql';
 
 export const parse = (query) => {
   return alasql(query);
