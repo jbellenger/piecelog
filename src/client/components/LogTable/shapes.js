cdb49aa13834a2894183c8e1372bc717840fb65e
jbellenger <github@james.bellenger.org>
2016-04-07 08:55:25 -0700

    shapes fix

--- src/client/components/LogTable/shapes.js
+++ src/client/components/LogTable/shapes.js
@@ -1,4 +1,3 @@
 import {PropTypes} from 'react';
-import LogEntry from '../../modules/model/LogEvent';
 
-export const LogRowsShape = PropTypes.arrayOf(PropTypes.instanceOf(LogEntry));
+export const LogRowsShape = PropTypes.arrayOf(PropTypes.object);
