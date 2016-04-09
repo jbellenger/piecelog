e3bad191941687cdf75aa389ee34a63469bfb880
jbellenger <github@james.bellenger.org>
2016-04-08 18:13:54 -0700

    median unit tests

--- src/client/modules/stats/__tests__/index-test.js
+++ src/client/modules/stats/__tests__/index-test.js
@@ -11,4 +11,19 @@ suite('Stats', () => {
       assert.propertyVal(stats, 'median', 7);
     });
   });
+
+  suite('median', () => {
+    test('returns undefined for empty array', () => {
+      assert.isUndefined(Stats.median([]));
+    });
+    test('returns correct value for single-item array', () => {
+      assert.equal(Stats.median([42]), 42);
+    });
+    test('returns correct value for even-count arrays', () => {
+      assert.equal(Stats.median([1, 2, 3, 4]), 3);
+    });
+    test('returns correct value for odd-count arrays', () => {
+      assert.equal(Stats.median([2, 3, 4]), 3);
+    });
+  });
 });
