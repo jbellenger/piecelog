dd2ff548f4b4f587ce65e508a4512a93c3a20925
jbellenger <github@james.bellenger.org>
2016-04-17 05:48:10 -0700

    add missing components

---
+++ src/client/components/SparkLine/index.js
@@ -0,0 +1,12 @@
+import React,{PropTypes} from 'react';
+import styles from './styles.css';
+
+export default class SparkLine extends React.Component {
+  render() {
+    return (
+      <svg className={styles.root}>
+        <rect />
+      </svg>
+    );
+  }
+};
---
+++ src/client/components/SvgTest/Route.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import SvgTest from '.';
+
+export default (props) => <SvgTest />;
