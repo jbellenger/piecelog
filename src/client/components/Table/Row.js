3a729a74d65144b1a473a96007b79e198a464c34
jbellenger <github@james.bellenger.org>
2016-05-24 17:59:42 -0700

    start implementing rowspans

--- src/client/components/Table/Row.js
+++ src/client/components/Table/Row.js
@@ -10,9 +10,29 @@ export default class Row extends React.Component {
   render() {
     const {fields, row} = this.props;
 
+    // field extractor protocol:
+    //   - if extractor returns an object, then:
+    //     - object will be formatted directly by field
+    //     - surrounding rows will have normal colspan
+    //
+    //   - if extractor returns an ??? (array? "MultiRowField"?) then:
+    //     - sub items will be individually formatted by field
+    //     - surrounding rows will have colspan incremented by size of array
+
+    const objs = fields.map((field, i) => {
+      const e = field.extractor(row);
+      return {
+        field: field,
+        index: i,
+        formatted: field.formatter(e),
+        extracted: e,
+        rowspan: 1,
+      };
+    });
+
     return (
       <tr>
-        {fields.map((field,i) => <td key={i}>{field.apply(row)}</td>)}
+        {objs.map((obj) => <td rowSpan={obj.rowspan} key={obj.index}>{obj.formatted}</td>)}
       </tr>
     );
   }
