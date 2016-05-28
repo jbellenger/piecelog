9a3461558f8b757ff9e91d8e29dc68446b7e3134
jbellenger <github@james.bellenger.org>
2016-05-28 06:00:03 -0700

    fix missing field

--- src/client/components/ResultsTable/index.js
+++ src/client/components/ResultsTable/index.js
@@ -38,7 +38,6 @@ export default class ResultsTable extends React.Component {
     const fields = [
       Fields.RESULT_PERSON_ID,
       Fields.RESULT_WEIGHT_POUNDS,
-      Fields.RESULT_ENTRY_INDEX,
       Fields.RESULT_ENTRY_SPLIT,
       Fields.RESULT_ENTRY_WATTS_PER_KG,
     ];
