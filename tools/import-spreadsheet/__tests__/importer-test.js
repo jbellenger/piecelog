f27c02f68ef70fff03355b04886e3e2537f9d680
jbellenger <github@james.bellenger.org>
2016-06-02 07:06:54 -0700

    update test

--- tools/import-spreadsheet/__tests__/importer-test.js
+++ tools/import-spreadsheet/__tests__/importer-test.js
@@ -16,9 +16,9 @@ suite('importer', () => {
   suite('extractPeople', () => {
     test('extracts people', () => {
       const input = [
-          {log_person_id: 'name-1', log_racingage: 1, log_piece_id: 'piece-1'},
-          {log_person_id: 'name-1', log_racingage: 2, log_piece_id: 'piece-2'},
-          {log_person_id: 'name-2', log_racingage: 3, log_piece_id: 'piece-1'},
+          {person_id: 'name-1', racingage: 1, piece_id: 'piece-1'},
+          {person_id: 'name-1', racingage: 2, piece_id: 'piece-2'},
+          {person_id: 'name-2', racingage: 3, piece_id: 'piece-1'},
       ];
 
       const result = importer.extractPeople(input);
@@ -32,8 +32,8 @@ suite('importer', () => {
   suite('racingDob', () => {
     test('extracts utc dobs', () => {
       const row = {
-        log_stamp: new Date(Date.parse('2015-08-22')),
-        log_racingage: 34,
+        stamp: new Date(Date.parse('2015-08-22')),
+        racingage: 34,
       };
       const dob = importer.racingDob(row);
       assert.equal(dob.getTime(), Date.parse('1981-01-01'));
