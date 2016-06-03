408392e77b0e74eb9b1991ce312e0ead4529a7b6
jbellenger <github@james.bellenger.org>
2016-06-02 17:57:58 -0700

    simple Workouts index view

--- src/client/components/Router/index.js
+++ src/client/components/Router/index.js
@@ -5,6 +5,7 @@ import About from '../About';
 import Home from '../Home';
 import PersonRoute from '../Person/PersonRoute';
 import WorkoutRoute from '../Workout/WorkoutRoute';
+import WorkoutIndexView from '../Workout/WorkoutIndexView';
 import EventRoute from '../Event/EventRoute';
 import SvgTest from '../SvgTest/Route';
 
@@ -18,7 +19,11 @@ export default class PageRouter extends React.Component {
           <Route path="about" component={About} />
           <Route path="svg" component={SvgTest} />
           <Route path="person/:personId" component={PersonRoute} />
-          <Route path="workout/:workoutId" component={WorkoutRoute} />
+          <Route path="workout">
+            <IndexRoute component={WorkoutIndexView} />
+            <Route path=":workoutId" component={WorkoutRoute} />
+          </Route>
+
           <Route path="event/:eventId" component={EventRoute} />
         </Route>
       </Router>
