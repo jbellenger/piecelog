e2f2cdb64985b2337a41ffbffdbefe624cefd186
jbellenger <github@james.bellenger.org>
2016-06-04 06:46:57 -0700

    wip victory experimentatio

--- src/client/components/GraphTest/index.js
+++ src/client/components/GraphTest/index.js
@@ -1,5 +1,26 @@
 import React from 'react';
 import styles from './styles.css';
 import {VictoryChart} from 'victory';
+import {connect} from 'react-redux';
+import {selector as modelsSelector} from '../../modules/store/models';
+import * as ResultsFields from '../ResultsTable/fields';
 
-export default (props) => <VictoryChart />;
+const View = ({results}) => {
+  console.log('results', results);
+  return(
+    <VictoryChart 
+      data={results}
+      x={ResultsFields.STAMP.extractor}
+      y={ResultsFields.ENTRY_SPLIT.extractor}
+    />
+  );
+};
+
+export const mapStateToProps = (state) => {
+  const models = modelsSelector(state);
+  return {
+    results: models.results.filterByWorkoutId('6k'),
+  };
+};
+
+export default connect(mapStateToProps)(View);
