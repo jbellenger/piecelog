700192c43505458fa587e086e760acc91a1f93e6
jbellenger <github@james.bellenger.org>
2016-06-10 07:06:28 -0700

    more detail in graph

--- src/client/components/Person/PersonView.js
+++ src/client/components/Person/PersonView.js
@@ -2,6 +2,9 @@ import React, {PropTypes} from 'react';
 import {connect} from 'react-redux';
 import minBy from 'lodash/minBy';
 import maxBy from 'lodash/maxBy';
+import flatten from 'lodash/flatten';
+import groupBy from 'lodash/groupBy';
+import toPairs from 'lodash/toPairs';
 import {selector as modelsSelector} from '../../modules/store/models';
 import ResultsTable from '../ResultsTable';
 import * as ResultFields from '../ResultsTable/fields';
@@ -13,31 +16,59 @@ export class PersonView extends React.Component {
     person: PropTypes.object.isRequired
   };
 
+  renderMultiPieceLines([workoutId, results]) {
+    return results
+      .filter((r) => r.entry_collection.entries.length > 1)
+      .map((r) => (
+        <VictoryLine
+          data={r.entry_collection.entries}
+          x={() => r.stamp.getTime()}
+          y={"split_seconds"}
+        />
+      ));
+  }
+
+  renderScatter([workoutId, results]) {
+    return results
+      .map((r) => (
+        <VictoryScatter
+          data={r.entry_collection.entries}
+          x={() => r.stamp.getTime()}
+          y={"split_seconds"}
+        />
+      ));
+  }
+
+  renderMultiWorkoutLines([workoutId, results]) {
+    return (
+      <VictoryLine
+        data={results}
+        x={(r) => r.stamp.getTime()}
+        y={(r) => r.entry_collection.mean.split_seconds}
+      />
+    );
+  }
+
   render() {
     const {person, results} = this.props;
+
     const stamps = results.map((r) => r.stamp);
-    const domain = {x: [Math.min(...stamps), Math.max(...stamps)]}; 
+    const splits = flatten(results.map((r) => r.entry_collection.entries.map((e) => e.split_seconds)));
+    const domain = {
+      x: [Math.min(...stamps), Math.max(...stamps)],
+      y: [Math.min(...splits), Math.max(...splits)]
+    }; 
+
+    const workoutGroups = groupBy(results, 'workout_id');
+
     return (
       <div>
         <h1>{person.id}</h1>
         <div>
           <VictoryChart domain={domain}>
-            {results.map((r) => (
-              <VictoryScatter
-                data={r.entry_collection.entries}
-                x={() => r.stamp.getTime()}
-                y={"split_seconds"}
-              />
-            ))}
-            {results
-              .filter((r) => r.entry_collection.entries.length > 1)
-              .map((r) => (
-                <VictoryLine
-                  data={r.entry_collection.entries}
-                  x={() => r.stamp.getTime()}
-                  y={"split_seconds"}
-                />
-            ))}
+            {toPairs(workoutGroups).map(this.renderMultiPieceLines)}
+            {toPairs(workoutGroups).map(this.renderScatter)}
+            {toPairs(workoutGroups).map(this.renderMultiWorkoutLines)}
 
             <VictoryAxis
               label={ResultFields.STAMP.header}
